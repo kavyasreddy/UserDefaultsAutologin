39bb0e83f341792efed7d0a0f63f41d0ba70f9f5
kavya <kavyasreddy55@gmail.com>
2023-11-21 14:16:39 +0530

    Initial Commit

---
+++ UserDefaultsAutologin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserDefaultsAutologin
+//
+//  Created by Thonduru Kavya on 1945-08-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
